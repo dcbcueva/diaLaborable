5fb9451ee51c43430a01521214b09c8195309cb9
Diego Bonilla <dcbcueva@gmail.com>
2022-09-27 23:01:11 -0500

    ci:pruebas js

--- script.js
+++ script.js
@@ -5,19 +5,21 @@ const inputRes = document.getElementById("dma")
 
 const buttonCalc = document.getElementById("calc")
 
-/*
-const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-const d = Date(`${b},${a},${c}`) => weekday[d.getDay()] 
-const dia = d.getDay()
-*/
-//const di = (a, b, c) => "b" + "a" + "c".getDay()
-const di = (a,b,c) => Date(`${b} ${a} ${c}`) //=> di.getDay()
-const rest = (a, b) => a - b
-const mult = (a, b) => a * b
-const divi = (a, b) => a / b
+const dias = ["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sabado"];
+const diaLaborable = (diaSemana) => { 
+    if (diaSemana>=1&&diaSemana<=5) return"día laborable"
+    else return "fin de semana"
+} 
 
-const operacionDia = () => {
-    inputRes.value = di(inputDia.value,inputMes.value,inputAño.value)
+const dia = () => { 
+    const di = new Date(inputAño.value, inputMes.value - 1, inputDia.value)
+    const diaSemana = di.getDay()
+    inputRes.value = `${dias[diaSemana]}, ${diaLaborable(diaSemana)}`
+}
+
+const operacionDia = event => {
+    event.preventDefault()
+    inputDia.value&&inputMes.value&&inputAño.value ? dia() : alert("Verifica los datos") 
 }
 
 buttonCalc.addEventListener("click", operacionDia)
